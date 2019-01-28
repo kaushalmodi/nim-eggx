e78251f83b14fa35628055e4f195b3e7233601da
Kaushal Modi <kaushal.modi@gmail.com>
2019-01-28 17:39:52 -0500

    Add demo/test

---
+++ tests/draw.nim
@@ -0,0 +1,44 @@
+import eggx
+from math import ln
+
+const
+  myColor = DS9_B
+
+var
+  Gwin: cint = 0
+
+proc draw_init*(width, height: cint): cint {.exportc.} =
+  gsetinitialparsegeometry("%+d%+d", Gwin * (200 + 10), 0)
+  let
+    win: cint = gopen(width, height) # The returned int value increments with each new window.
+  layer(win, 0, 1)
+  Gwin = win
+  echo "Gwin = ", Gwin
+  return win
+
+proc draw_clear*(win: cint) {.exportc.} =
+  gclr(win)
+
+proc draw_title*(win: cint; title: cstring) {.exportc.} =
+  discard winname(win, title)
+
+proc draw_pixel*(win, x, y, n, minlimit, maxlimit: cint) {.exportc.} =
+  var
+    first = 1
+    minl, maxl: float
+    color_r, color_g, color_b: cint
+
+  if first==1:
+    minl = ln(float(minlimit))
+    maxl = ln(float(maxlimit))
+    first = 0
+
+  discard makecolor(myColor, cfloat(minl), cfloat(maxl), cfloat(ln(float(n))), addr color_r, addr color_g, addr color_b)
+  newrgbcolor(win, color_r, color_g, color_b)
+  pset(win, x.cfloat, y.cfloat)
+
+proc draw_flush*(win: cint) {.exportc.} =
+  copylayer(win, 1, 0)
+
+proc draw_finish*(win: cint) {.exportc.} =
+  draw_flush(win)
---
+++ tests/tmandel.nim
@@ -0,0 +1,76 @@
+import draw
+import strformat
+from os import sleep
+
+proc hw_sync_placeholder(cnt: cint) = sleep(cnt)
+
+const
+  thres = 4.0
+  limit: cint = 1000
+
+# Mandel
+
+proc get_mandel(c_real, c_imaginary: float): int32 =
+  var
+    i: int32
+    re, im: float
+
+  while true:
+    let
+      xsq = re * re
+      ysq = im * im
+    if xsq + ysq > thres:
+      break
+    im = 2 * re * im + c_imaginary
+    re = xsq - ysq + c_real
+    i += 1
+    if i >= limit:
+      return 0
+  return i
+
+proc mandel*(winWidth: cint  = 600,
+             winHeight: cint = 600,
+             xScale          = 1.0,
+             yScale          = 1.0,
+             realBegin       = 0.075,
+             realEnd         = 0.175,
+             imagBegin       = 0.59,
+             imagEnd         = 0.69,
+             repeat: cint    = 1,
+             endPause: cint  = 3000,
+             hw_sync         = hw_sync_placeholder) =
+  let
+    modn = winWidth div 20
+    xstep = xScale * (realEnd - realBegin) / winWidth.float
+    ystep = yScale * (imagEnd - imagBegin) / winHeight.float
+    win = draw_init(winWidth, winHeight)
+    label = fmt"Nim (win={win}) IMG({realBegin}, {imagBegin}) ({realEnd}, {imagEnd})"
+
+  draw_title(win, cstring(label)) # convert label from string -> cstring
+
+  for _ in 1 .. repeat:
+    var
+      yreal = imagBegin
+    for y in 0.cint ..< winHeight:
+      hw_sync(1)
+      var
+        xreal = realBegin
+      for x in 0.cint ..< winWidth:
+        let
+          n = get_mandel(xreal, yreal)
+        if n > 0:
+           draw_pixel(win, x, y, n, 1, limit)
+        if y mod modn == 0:
+          draw_flush(win)
+        xreal += xstep
+      yreal += ystep
+
+    draw_finish(win)
+    hw_sync(endPause)
+    draw_clear(win)
+
+when isMainModule:
+  mandel()
+
+# Adapted from the EGGX sample distribution:
+#   http://phe.phyas.aichi-edu.ac.jp/~cyamauch/eggx_procall
